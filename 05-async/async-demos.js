aea518332f770de5a8f37a1c709431fc71b2732e
Magesh <tkmagesh77@gmail.com>
2019-04-10 16:55:23 +0530

    promise usage

--- 05-async/async-demos.js
+++ 05-async/async-demos.js
@@ -33,4 +33,58 @@
 
 	window['addAsyncClient'] = addAsyncClient;
 
+	function addAsyncPromise(x,y){
+		console.log(`	[@service] processing ${x} and ${y}`);
+		
+		var p = new Promise(function(resolveFn, rejectFn){
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`	[@service] returning result`);
+				//return result;
+				resolveFn(result);
+			}, 5000);
+		});
+
+		return p;
+	}
+
+	window['addAsyncPromise'] = addAsyncPromise;
+
 })();
+
+/*
+Client usage
+*/
+var p = addAsyncPromise(10,20);
+
+var p2 = p.then(function(result){
+	console.log(`[@Client] result = ${result}`);
+	var p2 = new Promise(function(resolveFn, rejectFn){
+		setTimeout(function(){
+			var doubleResult = result * 2;
+			resolveFn(doubleResult);
+			console.log('doubleResult done');
+    	},5000);
+    });
+	return p2;
+})
+
+var p2 = p.then(function(result){
+	console.log(`[@Client] result = ${result}`);
+	return new Promise(function(resolveFn, rejectFn){
+		var doubleResult = result * 2;
+		resolveFn(doubleResult);
+    });
+})
+
+var p2 = p.then(function(result){
+	console.log(`[@Client] result = ${result}`);
+	var doubleResult = result * 2;
+	return Promise.resolve(doubleResult);
+})
+
+var p2 = p.then(function(result){
+	console.log(`[@Client] result = ${result}`);
+	var doubleResult = result * 2;
+	return doubleResult;
+})

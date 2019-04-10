e5265bef78074c22500e9c2749f71128d68cf2cc
Magesh <tkmagesh77@gmail.com>
2019-04-10 10:53:40 +0530

    Handling payload and replacing state-manager with redux

--- 03-state-manager/state-manager.js
+++ 03-state-manager/state-manager.js
@@ -30,5 +30,16 @@ var SM = (function(){
 		return store;
 	}
 
-	return { createStore };
+	function bindActionCreators(actionCreators, dispatch){
+		let actionDispatchers = {};
+		for(let key in actionCreators){
+			actionDispatchers[key] = function(...args){
+				let action = actionCreators[key](...args);
+				dispatch(action);
+			}
+		}
+		return actionDispatchers;
+	}
+
+	return { createStore, bindActionCreators };
 })();
